173c43d6439e9c01e29ebf339c4eba45c8b98d5f
zhouyw <zhouyw18087@163.com>
2022-05-06 16:31:13 +0800

    second commit

--- rampup_data/src/main/java/com/demo/rampup/data/generator/CodeGenerator.java
+++ rampup_data/src/main/java/com/demo/rampup/data/generator/CodeGenerator.java
@@ -45,7 +45,7 @@ public class CodeGenerator {
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
         String projectPath = System.getProperty("user.dir");
-        gc.setOutputDir(projectPath + "/easy_park_data" + "/src/main/java");
+        gc.setOutputDir(projectPath + "/rampup_data" + "/src/main/java");
         gc.setAuthor("zhouyw");
         gc.setOpen(false);
         // gc.setSwagger2(true); 实体属性 Swagger2 注解
@@ -53,11 +53,11 @@ public class CodeGenerator {
 
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql://192.168.1.252:3306/easy_park?useUnicode=true&useSSL=false&characterEncoding=utf8&serverTimezone=UTC");
+        dsc.setUrl("jdbc:mysql://127.0.0.1:3306/test?useUnicode=true&useSSL=false&characterEncoding=utf8&serverTimezone=UTC");
         // dsc.setSchemaName("public");
         dsc.setDriverName("com.mysql.cj.jdbc.Driver");
         dsc.setUsername("root");
-        dsc.setPassword("nesun123");
+        dsc.setPassword("root");
         mpg.setDataSource(dsc);
 
         // 包配置
@@ -86,7 +86,7 @@ public class CodeGenerator {
             @Override
             public String outputFile(TableInfo tableInfo) {
                 // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-                return projectPath + "/easy_park_data" + "/src/main/resources/mapper/" + pc.getModuleName()
+                return projectPath + "/rampup_data" + "/src/main/resources/mapper/" + pc.getModuleName()
                         + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
             }
         });
